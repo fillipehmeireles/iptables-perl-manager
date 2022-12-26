2edefd8a89116331a0bbf2bf6507945b044d53b1
Fillipe Meireles <fillipemeireles@localhost.localdomain>
2022-12-26 10:54:33 -0300

    initial commit

--- firewall.pl
+++ firewall.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl
 
-package PerlIPtables;
 
 use IPTables::ChainMgr;
 use YAML::XS qw(LoadFile);
@@ -18,7 +17,7 @@ $firewall_config = LoadFile('firewall.yaml');
 sub default_filter_policy {
 		my $table = $tables{FILTER};
 		my @chains = ('INPUT','FORWARD','OUTPUT');
-		my $policy = 'DROP';
+		my $policy = 'ACCEPT';
 		foreach (@chains) {
 			($rv) = ($_[0]->set_chain_policy($table,  $_ , $policy))[0];
 			$rv == 0 ? print "[!] ($table/$_) > Error on Setting policy $policy \n" : print "[*] ($table/$_) > Setting policy $policy \n";
@@ -31,6 +30,11 @@ sub ssh_access {
 	($rv, $out_ar, $errs_ar) = $_[0]->append_ip_rule('0/0',
     '0/0', $tables{FILTER}, $chain, $policy,
     {'protocol' => 'tcp','d_port' => 22});
+}
+
+sub show_filter_rules {
+	my ($rv, $out_ar, $errs_ar) = $_[0]->run_ipt_cmd("${ipt_bin} -nvL");
+	print "\n@$out_ar";
 }
 
 %options = (
@@ -46,4 +50,5 @@ sub ssh_access {
 $ipt = IPTables::ChainMgr->new(%options) or die "[!] Could not acquire IPTables obj";
 
 default_filter_policy($ipt);
-
+show_filter_rules($ipt);
+ssh_access($ipt);
